223701f68c66d6ebe3aa88daaeed817a91afd5fb
Tien Dung <tiendung.dev12@gmail.com>
2021-11-22 15:35:42 +0700

    initial commit

--- demo_ver1/Program.cs
+++ demo_ver1/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.AspNetCore.Components;
 using demo_ver1.HubClients;
-
+using demo_ver1.Services.MapView;
 using demo_ver1;
 
 
@@ -13,9 +13,31 @@ using demo_ver1;
         builder.RootComponents.Add<App>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
 
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-        builder.Services.AddAntDesign();
+
+        builder.Services.AddScoped<RobotNetAuthorizationMessageHandler>();
+            builder.Services.AddHttpClient("RobotNetHttpClient", options =>
+        {
+            //options.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+            options.BaseAddress = new Uri(builder.Configuration.GetConnectionString("HostServer"));
+        })
+        .AddHttpMessageHandler<RobotNetAuthorizationMessageHandler>();
+            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("RobotNetHttpClient"));
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("OidcAuthentication", options.ProviderOptions);
+    options.ProviderOptions.DefaultScopes.Add("roles");
+    options.ProviderOptions.DefaultScopes.Add("profile");
+    options.ProviderOptions.DefaultScopes.Add("email");
+    options.ProviderOptions.DefaultScopes.Add("openid");
+    options.ProviderOptions.DefaultScopes.Add("RobotNetUserApiScope");
+});
+builder.Services.AddAntDesign();
         builder.Services.AddScoped<MapHubClients>();
+
+        builder.Services.AddSingleton<MapCommon>();
+        builder.Services.AddSingleton<MapCoordinateConversion>();
+        builder.Services.AddSingleton<MapViewer>();
+        builder.Services.AddSingleton<AreaMapProvider>();
 
 await builder.Build().RunAsync();
 
